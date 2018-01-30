9430d6ca62536443c843fd1dc7cb64281a1882db
Zachary Coursey <zcoursey22@gmail.com>
2018-01-30 20:00:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,10 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(arr, e) {
+  return arr.unshift(e)
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, e) {
+  arr.unshift(e)
+  return arr
+}
