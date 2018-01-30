da0e0ba0fbf7c8d493081e79d3db5ccdade068d5
Zachary Coursey <zcoursey22@gmail.com>
2018-01-30 22:25:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,3 +8,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array
 }
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
