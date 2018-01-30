77d70d4baa1326bdcaae81860603018a7ccdc565
Zachary Coursey <zcoursey22@gmail.com>
2018-01-30 22:30:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,5 +22,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  return array.pop()
+  array.pop()
+  return array
 }
